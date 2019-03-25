29d7a786a5f26a2028cced6d2628194bedf5b5b8
DevilDGL <44584041+DevilDGL@users.noreply.github.com>
2019-03-25 11:39:47 +0100

    Add files via upload

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAypWS-eDx-ULm0MgDT6tYTut97Qyhx_Kk",
+    authDomain: "agenda-ef34c.firebaseapp.com",
+    databaseURL: "https://agenda-ef34c.firebaseio.com",
+    projectId: "agenda-ef34c",
+    storageBucket: "agenda-ef34c.appspot.com",
+    messagingSenderId: "1067913931560"
+};
